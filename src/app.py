11b6d7e4e5b9e630d39d5da5289ae3717a77e790
Felipe Ribeiro <134939651+fribeir0@users.noreply.github.com>
2025-10-28 10:06:22 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Simulação de um banco de dados (lista)
 usuarios = [
     {"id": 1, "nome": "Jhennifer", "email": "jhennifer@email.com"},
-    {"id": 2, "nome": "Samuel", "email": "samuel@email.com"}
+    {"id": 2, "nome": "Samuel", "email": "samuel@2email.com"}
     {"id": 3, "nome": "Felipe", "email": "felipeeee@email.com"}
 ]
 
