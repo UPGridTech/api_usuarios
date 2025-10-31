6bbdbe3a0f417c5726ac83bd9054a1d5d66f6826
Felipe Ribeiro <134939651+fribeir0@users.noreply.github.com>
2025-10-31 09:31:15 -0300

    Fix email format for user Samuel

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 # Simulação de um banco de dados (lista)
 usuarios = [
     {"id": 1, "nome": "Jhennifer", "email": "jhennifer222@email.com"},
-    {"id": 2, "nome": "Samuel", "email": "samuel2222@email.com"}
+    {"id": 2, "nome": "Samuel", "email": "samuel22222@email.com"}
     {"id": 3, "nome": "Felipe", "email": "felipeee2222e@email.com"}
 ]
 
