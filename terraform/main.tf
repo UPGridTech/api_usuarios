0e1dd123152e60d5834ff82d3e817a50ede5217f
Felipe Ribeiro <134939651+fribeir0@users.noreply.github.com>
2025-11-25 12:36:41 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "bkt-senai-02"
-    key            = "envs/vm-${var.vm_id}/terraform.tfstate"
+    bucket         = "bkt-senai-02"    # troque pelo output bucket_name
+    key            = "envs/pr-${var.pr_number}/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "bkt-senai-02-lock"
     encrypt        = true
@@ -23,47 +23,48 @@ provider "vsphere" {
   api_timeout          = 300
 }
 
-data "vsphere_datacenter" "dc" {
+data "vsphere_datacenter" "datacenter" {
   name = var.vsphere_datacenter
 }
 
 data "vsphere_datastore" "datastore" {
   name          = var.vsphere_datastore
-  datacenter_id = data.vsphere_datacenter.dc.id
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_compute_cluster" "cluster" {
   name          = var.vsphere_cluster_name
-  datacenter_id = data.vsphere_datacenter.dc.id
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_network" "network" {
-  name          = var.vsphere_network
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = var.mgmt_lan
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_virtual_machine" "template" {
   name          = var.template_name
-  datacenter_id = data.vsphere_datacenter.dc.id
+  datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-resource "vsphere_virtual_machine" "vm" {
-  name             = var.vm_id
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = data.vsphere_datastore.datastore.id
-
-  num_cpus = var.num_cpu
-  memory   = var.ram_memory
+resource "vsphere_virtual_machine" "virtualmachine" {
+  count             = var.vm_count
+  name = format("%s-%02d-%s", var.vm_name_base, count.index + 1, formatdate("YYYYMMDD-HHmmss", timestamp()))
+  resource_pool_id  = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id      = data.vsphere_datastore.datastore.id
+  force_power_off   = false
+  num_cpus          = var.num_cpu
+  memory            = var.ram_memory
 
   guest_id  = data.vsphere_virtual_machine.template.guest_id
   scsi_type = data.vsphere_virtual_machine.template.scsi_type
-
   network_interface {
     network_id = data.vsphere_network.network.id
   }
 
+
   disk {
-    label            = "disk0"
+    label            = format("disk0-%02d", count.index + 1)
     size             = data.vsphere_virtual_machine.template.disks[0].size
     eagerly_scrub    = data.vsphere_virtual_machine.template.disks[0].eagerly_scrub
     thin_provisioned = lookup(data.vsphere_virtual_machine.template.disks[0], "thin_provisioned", true)
@@ -71,9 +72,10 @@ resource "vsphere_virtual_machine" "vm" {
 
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
+    }
   }
-}
 
-output "vm_ip" {
-  value = vsphere_virtual_machine.vm.default_ip_address
+  output "vm_ip" {
+  description = "IP da VM criada"
+  value       = [for vm in vsphere_virtual_machine.virtualmachine : vm.default_ip_address]
 }
